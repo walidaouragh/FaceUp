5512ffcee348ac6306a1a44e35604231b0f27897
walidaouragh <34663591+walidaouragh@users.noreply.github.com>
2021-02-15 15:48:37 -0500

    Add Identity and rolemanagement

--- Faceup.API/Entities/AppUser.cs
+++ Faceup.API/Entities/AppUser.cs
@@ -1,16 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace Faceup.API.Entities
 {
-    [Table("Users")]
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
@@ -26,5 +22,6 @@ namespace Faceup.API.Entities
         public ICollection<UserLike> Likees { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
